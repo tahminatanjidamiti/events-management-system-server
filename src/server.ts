1cb711032015a188981c1407ada39148e8f664d9
tahminatanjidamiti <mity2027@gmail.com>
2025-12-07 03:57:30 +0600

    setup completed

---
+++ src/server.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+import http, { Server } from "http";
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let server: Server | null = null;
+async function connectToDB() {
+  try {
+    console.log("*** DB connection successfull!!")
+  } catch (error) {
+    console.log("*** DB connection failed!", error)
+    process.exit(1);
+  }
+}
+
+async function startServer() {
+  try {
+    await connectToDB()
+    server = http.createServer(app);
+    server.listen(process.env.PORT, () => {
+      console.log(`🚀 Server is running on port ${process.env.PORT}`);
+    });
+
+    handleProcessEvents();
+  } catch (error) {
+    console.error("❌ Error during server startup:", error);
+    process.exit(1);
+  }
+}
+
+async function gracefulShutdown(signal: string) {
+  console.warn(`🔄 Received ${signal}, shutting down gracefully...`);
+
+  if (server) {
+    server.close(async () => {
+      console.log("✅ HTTP server closed.");
+
+      try {
+        console.log("Server shutdown complete.");
+      } catch (error) {
+        console.error("❌ Error during shutdown:", error);
+      }
+
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+}
+
+function handleProcessEvents() {
+  process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+  process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+
+  process.on("uncaughtException", (error) => {
+    console.error("💥 Uncaught Exception:", error);
+    gracefulShutdown("uncaughtException");
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    console.error("💥 Unhandled Rejection:", reason);
+    gracefulShutdown("unhandledRejection");
+  });
+}
+
+startServer()
